9359dc47ec62bcf9f3db5bdeb796b8e602ae99ca
PluginOfTimes <moritz_gcg@web.de>
2023-09-23 15:52:51 +0200

    Improved parameterized component factory

--- Snowflax/src/Snowflax/IoC/Container.h
+++ Snowflax/src/Snowflax/IoC/Container.h
@@ -7,6 +7,13 @@
 #include <memory>
 #include <format>
 
+template<class T>
+concept Parameterized = requires() {
+    { typename T::IoCParams{} };
+};
+template<class T>
+concept NotParameterized = !Parameterized<T>;
+
 
 class Container {
 public:
@@ -15,35 +22,40 @@ public:
     template<class T>
     using ParameterizedGenerator = std::function<std::shared_ptr<T>(typename T::IoCParams params)>;
 
-    template<class T>
+    template<NotParameterized T>
     void RegisterFactory(Generator<T> gen) {
         factoryMap[typeid(T)] = gen;
     }
-    template<class T>
+    template<Parameterized T>
     void RegisterFactory(ParameterizedGenerator<T> gen) {
         factoryMap[typeid(T)] = gen;
     }
 
-    template<class T>
+    template<NotParameterized T>
     std::shared_ptr<T> Resolve() {
-        if(const auto i = factoryMap.find(typeid(T); i != factoryMap.end())) {
-            return std::any_cast<Generator<T>>(i->second)();
-        }
-        else {
-            throw std::runtime_error { std::format("Could not find generator for type [{}] in factory map!", typeid(T).name()) };y
-        }
+        return m_Resolve<T, Generator<T>>();
     }
-    template<class T>
-    std::shared_ptr<T> Resolve(typename T::IoCParams params) {
+    template<Parameterized T>
+    std::shared_ptr<T> Resolve(typename T::IoCParams&& params = {}) {
+        return m_Resolve<T, ParameterizedGenerator<T>>(std::forward<typename T::IoCParams>(params));
+    }
+
+    static Container& Get() {
+        static Container c;
+        return c;
+    }
+private:
+    template<class T, class G, class...Ps>
+    std::shared_ptr<T> m_Resolve(Ps&&...arg) {
         if (const auto i = factoryMap.find(typeid(T); i != factoryMap.end())) {
             try {
-                return std::any_cast<ParameterizedGenerator<T>>(i->second)(std::move(params));
+                return std::any_cast<G>(i->second)(std::forward<Ps>(arg)...);
             }
-            catch (const std::bad_any_cast) {
+            catch (const std::bad_any_cast&) {
                 throw std::runtime_error{
                     std::format(
                         "Could not resolve Services mapped type\nfrom: [{}]\n to: [{}]\n",
-                        i->second.type().name(), typeid(ParameterizedGenerator<T>).name()
+                        i->second.type().name(), typeid(G).name()
                     );
                 }
             }
@@ -53,10 +65,5 @@ public:
         }
     }
 
-    static Container& Get() {
-        static Container c;
-        return c;
-    }
-private:
     std::unordered_map<std::type_index, std::any> factoryMap;
 };
