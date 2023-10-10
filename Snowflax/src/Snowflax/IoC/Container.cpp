4a2c397b2034343568d4922479cce695d8d8c939
PluginOfTimes <moritz_gcg@web.de>
2023-10-10 18:54:15 +0200

    Moved functions from IoC/Container.h to IoC/Container.cpp

--- Snowflax/src/Snowflax/IoC/Container.cpp
+++ Snowflax/src/Snowflax/IoC/Container.cpp
@@ -1,1 +1,80 @@
 #include "Container.h"
+
+
+namespace Snowflax {
+
+    namespace Infrastructure {
+
+        template<NotParameterized T>
+        void Container::RegisterFactory(Generator<T> gen) {
+            m_factoryMap[typeid(T)] = { State::InstanceGenerator, std::move(gen) };
+        }
+        template<Parameterized T>
+        void Container::RegisterFactory(ParameterizedGenerator<T> gen) {
+            m_factoryMap[typeid(T)] = { State::InstanceGenerator, std::move(gen) };
+        }
+        template<class T>
+        void Container::RegisterSingleton(Generator<T> gen) {
+            m_factoryMap[typeid(T)] = { State::SingletonGenerator, std::move(gen) };
+        }
+
+        template<NotParameterized T>
+        std::shared_ptr<T> Container::Resolve() {
+            return m_Resolve<T, Generator<T>>();
+        }
+        template<Parameterized T, class...Ps>
+        std::shared_ptr<T> Container::Resolve(Ps&&...arg) {
+            return m_Resolve<T, ParameterizedGenerator<T>>(std::forward<Ps>(arg)...);
+        }
+
+        template<class T, class G, class...Ps>
+        std::shared_ptr<T> Container::m_Resolve(Ps&&...arg) {
+            if (const auto i = m_factoryMap.find(typeid(T)); i != m_factoryMap.end()) {
+                auto& entry = i->second;
+                if (entry.state != State::InstanceGenerator && sizeof...(Ps) > 0) {
+                    throw std::runtime_error{ "Parameter passed in while resolving a singleton!" };
+                }
+                else if (entry.state == State::InstanceGenerator && Parameterized<T> && sizeof...(Ps) == 0) {
+                    throw std::runtime_error{ "No parameters passed while resolving an independent instance!" };
+                }
+                try {
+                    std::shared_ptr<T> ptr;
+                    switch (entry.state) {
+                    case State::InstanceGenerator:
+                        ptr = std::any_cast<G>(entry.content)(std::forward<Ps>(arg)...);
+                        break;
+                    case State::SingletonGenerator:
+                        ptr = std::any_cast<G>(entry.content)(std::forward<Ps>(arg)...);
+                        entry.content = ptr;
+                        entry.state = State::SingletonInstance;
+                        break;
+                    case State::SingletonInstance:
+                        std::any_cast<std::shared_ptr<T>>(entry.content);
+                        break;
+                    default:
+                        break;
+                    }
+                    return ptr;
+                }
+                catch (const std::bad_any_cast&) {
+                    throw std::runtime_error{
+                        std::format(
+                            "Could not resolve Services mapped type\nfrom: [{}]\n to: [{}]\n",
+                            entry.content.type().name(), typeid(G).name()
+                        )
+                    };
+                }
+            }
+            else if (m_useContructor)
+                return std::make_shared<T>(std::forward<Ps>(arg)...);
+            else {
+                throw std::runtime_error{ std::format("Could not find generator for type [{}] in factory map!", typeid(T).name()) };
+            }
+        }
+
+        Container& Container::Get() {
+            static Container c;
+            return c;
+        }
+    }
+}
--- Snowflax/src/Snowflax/IoC/Container.h
+++ Snowflax/src/Snowflax/IoC/Container.h
@@ -1,5 +1,4 @@
 // TODO: Test all this shit!!!!!!
-//       Move all other than declerations to Container.cpp
 
 
 #pragma once
@@ -11,103 +10,55 @@
 #include <memory>
 #include <format>
 
-template<class T, class ...Ps>
-concept Parameterized = sizeof...(Ps) > 0;
-template<class T>
-concept NotParameterized = !Parameterized<T>;
+namespace Snowflax {
 
+    namespace Infrastructure {
 
-class Container {
-public:
+        template<class T, class ...Ps>
+        concept Parameterized = sizeof...(Ps) > 0;
+        template<class T>
+        concept NotParameterized = !Parameterized<T>;
 
-    template<class T>
-    using Generator = std::function<std::shared_ptr<T>()>;
-    template<class T, class...Ps>
-    using ParameterizedGenerator = std::function<std::shared_ptr<T>(Ps&&...arg)>;
+        class Container {
+        public:
 
+            template<class T>
+            using Generator = std::function<std::shared_ptr<T>()>;
+            template<class T, class...Ps>
+            using ParameterizedGenerator = std::function<std::shared_ptr<T>(Ps&&...arg)>;
 
-    template<NotParameterized T>
-    void RegisterFactory(Generator<T> gen) {
-        m_factoryMap[typeid(T)] = { State::InstanceGenerator, std::move(gen) };
+            template<NotParameterized T>
+            void RegisterFactory(Generator<T> gen);
+            template<Parameterized T>
+            void RegisterFactory(ParameterizedGenerator<T> gen);
+            template<class T>
+            void RegisterSingleton(Generator<T> gen);
+            
+            template<NotParameterized T>
+            std::shared_ptr<T> Resolve();
+            template<Parameterized T, class...Ps>
+            std::shared_ptr<T> Resolve(Ps&&...arg);
+            
+            static Container& Get();
+
+        private:
+
+            enum class State {
+                InstanceGenerator,
+                SingletonGenerator,
+                SingletonInstance
+            };
+            struct Entry {
+                State state;
+                std::any content;
+            };
+
+            template<class T, class G, class...Ps>
+            std::shared_ptr<T> m_Resolve(Ps&&...arg);
+
+            std::unordered_map<std::type_index, Entry> m_factoryMap;
+            bool m_useContructor = false;
+
+        };
     }
-    template<Parameterized T>
-    void RegisterFactory(ParameterizedGenerator<T> gen) {
-        m_factoryMap[typeid(T)] = { State::InstanceGenerator, std::move(gen) };
-    }
-    template<class T>
-    void RegisterSingleton(Generator<T> gen) {
-        m_factoryMap[typeid(T)] = { State::SingletonGenerator, std::move(gen) };
-    }
-
-    template<NotParameterized T>
-    std::shared_ptr<T> Resolve() {
-        return m_Resolve<T, Generator<T>>();
-    }
-    template<Parameterized T, class...Ps>
-    std::shared_ptr<T> Resolve(Ps&&...arg) {
-        return m_Resolve<T, ParameterizedGenerator<T>>(std::forward<Ps>(arg)...);
-    }
-
-    static Container& Get() {
-        static Container c;
-        return c;
-    }
-private:
-    enum class State {
-        InstanceGenerator,
-        SingletonGenerator,
-        SingletonInstance
-    };
-    struct Entry {
-        State state;
-        std::any content;
-    };
-
-    template<class T, class G, class...Ps>
-    std::shared_ptr<T> m_Resolve(Ps&&...arg) {
-        if (const auto i = m_factoryMap.find(typeid(T)); i != m_factoryMap.end()) {
-            auto& entry = i->second;
-            if (entry.state != State::InstanceGenerator && sizeof...(Ps) > 0) {
-                throw std::runtime_error{ "Parameter passed in while resolving a singleton!" };
-            }
-            else if (entry.state == State::InstanceGenerator && Parameterized<T> && sizeof...(Ps) == 0) {
-                throw std::runtime_error{ "No parameters passed while resolving an independent instance!" };
-            }
-            try {
-                std::shared_ptr<T> ptr;
-                switch (entry.state) {
-                case State::InstanceGenerator:
-                    ptr = std::any_cast<G>(entry.content)(std::forward<Ps>(arg)...);
-                    break;
-                case State::SingletonGenerator:
-                    ptr = std::any_cast<G>(entry.content)(std::forward<Ps>(arg)...);
-                    entry.content = ptr;
-                    entry.state = State::SingletonInstance;
-                    break;
-                case State::SingletonInstance:
-                    std::any_cast<std::shared_ptr<T>>(entry.content);
-                    break;
-                default:
-                    break;
-                }
-                return ptr;
-            }
-            catch (const std::bad_any_cast&) {
-                throw std::runtime_error{
-                    std::format(
-                        "Could not resolve Services mapped type\nfrom: [{}]\n to: [{}]\n",
-                        entry.content.type().name(), typeid(G).name()
-                    )
-                };
-            }
-        }
-        else if (m_useContructor)
-            return std::make_shared<T>(std::forward<Ps>(arg)...);
-        else {
-            throw std::runtime_error{ std::format("Could not find generator for type [{}] in factory map!", typeid(T).name()) };
-        }
-    }
-
-    std::unordered_map<std::type_index, Entry> m_factoryMap;
-    bool m_useContructor = false;
-};
+}
